3e7a785b8fba31f01d11522c8940b3f0a99efd2d
Anish Prakash <anish1.prakash@gmail.com>
2026-07-29 10:05:39 +0530

    Add Word [A] from Codeforces

---
+++ 59/A - Word/solution.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+ 
+int main() {
+    char s[105];
+    if (scanf("%104s", s) != 1) {
+        return 0;
+    }
+ 
+    int upper = 0, lower = 0;
+    for (int i = 0; s[i] != '\0'; i++) {
+        if (isupper(s[i])) {
+            upper++;
+        } else {
+            lower++;
+        }
+    }
+ 
+    if (upper > lower) {
+        for (int i = 0; s[i] != '\0'; i++) {
+            s[i] = toupper(s[i]);
+        }
+    } else {
+        for (int i = 0; s[i] != '\0'; i++) {
+            s[i] = tolower(s[i]);
+        }
+    }
+ 
+    printf("%s
+", s);
+ 
+    return 0;
+}
