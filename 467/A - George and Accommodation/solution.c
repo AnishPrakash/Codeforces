c254d5016b8d6b2a0dd995575bf25d9797de8eed
Anish Prakash <anish1.prakash@gmail.com>
2026-08-08 23:48:05 +0530

    Add George and Accommodation [A] from Codeforces

---
+++ 467/A - George and Accommodation/solution.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int n, p, q, count = 0;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d %d", &p, &q);
+        if (q - p >= 2) count++;
+    }
+    printf("%d
+", count);
+    return 0;
+}
