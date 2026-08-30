8d5867ee7fa23caa4626608df488743ab4b794d5
Anish Prakash <anish1.prakash@gmail.com>
2026-08-30 10:13:17 +0530

    Add The New Year: Meeting Friends [A] from Codeforces

---
+++ 723/A - The New Year: Meeting Friends/solution.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ 
+int main() {
+    int a, b, c;
+    if (scanf("%d %d %d", &a, &b, &c) == 3) {
+        int max = a;
+        int min = a;
+        
+        if (b > max) max = b;
+        if (b < min) min = b;
+        
+        if (c > max) max = c;
+        if (c < min) min = c;
+        
+        printf("%d
+", max - min);
+    }
+    return 0;
+}
